9db1ad6f2b87a182229820defa7eb063fd3a5c27
Two <22344029+jayimillena@users.noreply.github.com>
2023-10-26 13:00:49 +0800

    3 control structure

---
+++ Activity1.java
@@ -0,0 +1,25 @@
+public class Activity1 
+{
+  public static void main(String[] args)
+  {
+    String nameOfMyBoss = "Henry";
+    String myName = "Jay";
+    System.out.println("Hello Boss "+ nameOfMyBoss);
+    System.out.println("My name is "+ myName);
+    System.out.println("I'm the next top CEO like you!");
+
+    if (myName == "Jay")
+    {
+      System.out.println("Welcome boss "+ myName);
+    }
+    else 
+    {
+      System.out.println("Who's that pokemon!");      
+    }
+
+    for (int offering = 0; offering <= 3000000; offering++)
+    {
+      System.out.println(offering+ "000 Ikaw lang ang aking gusto");
+    }
+  }
+}
